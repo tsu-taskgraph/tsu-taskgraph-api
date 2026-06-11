336bb062c1c1324c3acb148635b1f555082af86a
Berezovskii Semyon <ber2semen@gmail.com>
2026-06-11 15:26:05 +0700

    Создание проекта, настройка `build.gradle`, подключение базовых зависимостей.

---
+++ src/main/java/ru/tsu_taskgraph/core_api/TsuTaskgraphApiApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu_taskgraph.core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsuTaskgraphApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsuTaskgraphApiApplication.class, args);
+    }
+
+}
