7da987d85d8abfdd8a42f2ade614e0e7884cec6c
Berezovskii Semyon <ber2semen@gmail.com>
2026-06-19 10:57:24 +0700

    поправил warning по маппингу

--- src/main/java/ru/tsu_taskgraph/core_api/mapper/ProjectMapper.java
+++ src/main/java/ru/tsu_taskgraph/core_api/mapper/ProjectMapper.java
@@ -2,10 +2,7 @@ package ru.tsu_taskgraph.core_api.mapper;
 
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.tsu_taskgraph.core_api.dto.project.ProjectDto;
-import ru.tsu_taskgraph.core_api.dto.project.ProjectMemberDto;
-import ru.tsu_taskgraph.core_api.dto.project.ProjectMetricsDto;
-import ru.tsu_taskgraph.core_api.dto.project.UpdateProjectRequest;
+import ru.tsu_taskgraph.core_api.dto.project.*;
 import ru.tsu_taskgraph.core_api.entity.Project;
 import ru.tsu_taskgraph.core_api.entity.ProjectMember;
 import ru.tsu_taskgraph.core_api.service.ProjectCalculationService;
@@ -16,11 +13,24 @@ public abstract class ProjectMapper {
     @Autowired
     private ProjectCalculationService calculationService;
 
+    @Mapping(source = "owner.id", target = "ownerId")
     public abstract ProjectDto toDto(Project project);
 
+    @Mapping(source = "project.id", target = "projectId")
+    @Mapping(source = "user.id", target = "userId")
     public abstract ProjectMemberDto toMemberDto(ProjectMember member);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "enrichmentStatus", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "teamSize", ignore = true)
+    @Mapping(target = "totalEstimatedHours", ignore = true)
+    @Mapping(target = "totalLoggedHours", ignore = true)
+    @Mapping(target = "completionPercent", ignore = true)
+    @Mapping(target = "members", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     public abstract void updateProjectFromDto(UpdateProjectRequest dto, @MappingTarget Project project);
 
     @AfterMapping
